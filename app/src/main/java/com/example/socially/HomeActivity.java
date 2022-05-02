6112e4e7d3d7551e3827f6f92a5175167be70ff8
christancf <christanf6@gmail.com>
2022-05-03 03:51:40 +0530

    Files modified

--- app/src/main/java/com/example/socially/HomeActivity.java
+++ app/src/main/java/com/example/socially/HomeActivity.java
@@ -1,18 +1,45 @@
 package com.example.socially;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity";
     ImageView HomeIV, UsersIV, AddPostIV, ChatIV, ProfileIV;
+    FirebaseAuth mAuth;
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Posts");
+
+        // Define ColorDrawable object and parse color
+        // using parseColor method
+        // with color hash code as its parameter
+//        ColorDrawable colorDrawable
+//                = new ColorDrawable(Color.parseColor("#FFFFFF"));
+
+        // Set BackgroundDrawable
+//        actionBar.setBackgroundDrawable(colorDrawable);
+//        actionBar.
+
 
         HomeIV = findViewById(R.id.home);
         UsersIV = findViewById(R.id.users);
@@ -24,12 +51,7 @@ public class HomeActivity extends AppCompatActivity {
         HomeIV.setOnClickListener(view -> {
             Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
         });
-        UsersIV.setOnClickListener(view -> {
-            Toast.makeText(getApplicationContext(), "Users", Toast.LENGTH_SHORT).show();
-            /*
-            * startActivity(new Intent(getApplicationContext(), UserActivity.class));
-            * */
-        });
+        UsersIV.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(), UsersActivity.class)));
         AddPostIV.setOnClickListener(view -> {
             Toast.makeText(getApplicationContext(), "Add Post", Toast.LENGTH_SHORT).show();
         });
@@ -37,7 +59,7 @@ public class HomeActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "Chat", Toast.LENGTH_SHORT).show();
         });
         ProfileIV.setOnClickListener(view -> {
-            Toast.makeText(getApplicationContext(), "Profile", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
         });
     }
 }
