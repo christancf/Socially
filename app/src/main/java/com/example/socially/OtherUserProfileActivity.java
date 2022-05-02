548daec75f3888321e5fb16857a2e7674f4ececb
christancf <christanf6@gmail.com>
2022-05-03 04:55:25 +0530

    Other user profile Activity modified

--- app/src/main/java/com/example/socially/OtherUserProfileActivity.java
+++ app/src/main/java/com/example/socially/OtherUserProfileActivity.java
@@ -3,18 +3,29 @@ package com.example.socially;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OtherUserProfileActivity extends AppCompatActivity {
 
     TextView output;
-
+    ImageView chatIcon;
+    String UID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_user_profile);
 
         output = findViewById(R.id.output);
-        output.setText(getIntent().getStringExtra("UID"));
+        chatIcon = findViewById(R.id.chat_icon);
+
+        UID = getIntent().getStringExtra("UID");
+        output.setText(UID);
+
+        chatIcon.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(), "Chat Icon", Toast.LENGTH_SHORT).show();
+            //Chat Function
+        });
     }
 }
