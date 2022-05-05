41e6fef2a2556fd06657283202d032ed1cc816fe
christancf <christanf6@gmail.com>
2022-05-05 18:57:41 +0530

    ProfileActivity modified

--- app/src/main/java/com/example/socially/ProfileActivity.java
+++ app/src/main/java/com/example/socially/ProfileActivity.java
@@ -4,39 +4,62 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.fonts.Font;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private static final String TAG = "ProfileActivity";
-
+    ImageView profileImageIV;
     TextView emailTV;
     ActionBar actionBar;
 
-    FirebaseAuth mAuth;
+    //User Name
+    String userName;
 
+    FirebaseAuth mAuth;
+    private String firebaseURL = "https://socially-14fd2-default-rtdb.asia-southeast1.firebasedatabase.app";
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        //Setting up a transparent actionbar
         actionBar = getSupportActionBar();
-        actionBar.setTitle("Profile");
-
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        //Setting up a back arrow
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+        final Drawable upArrow;
+        upArrow = getResources().getDrawable(R.drawable.ic_baseline_arrow_back_24);
+        upArrow.setColorFilter(Color.parseColor("#000000"), PorterDuff.Mode.SRC_ATOP);
+        actionBar.setHomeAsUpIndicator(upArrow);
 
+        //Creating firebase instance
         mAuth = FirebaseAuth.getInstance();
-
-        emailTV = findViewById(R.id.emailTV);
 
     }
 
@@ -44,7 +67,21 @@ public class ProfileActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         Log.d(TAG, "checkUserStatus: function called. User = " + user);
         if(user != null){
-            emailTV.setText("Email: " + user.getEmail());
+            FirebaseDatabase db = FirebaseDatabase.getInstance(firebaseURL);
+            DatabaseReference ref = db.getReference("Users");
+            ref.child(user.getUid()).get().addOnCompleteListener(task -> {
+                if (!task.isSuccessful()) {
+                    Log.e("firebase", "Error getting data", task.getException());
+                }
+                else {
+                    Log.d("firebase", String.valueOf(task.getResult().getValue()));
+                    Map<String, Object> UserData = (Map<String, Object>) task.getResult().getValue();
+
+                    System.out.println(UserData.get("firstName"));
+                    userName = (String) UserData.get("firstName")+ " " +UserData.get("lastName");
+                    actionBar.setTitle(Html.fromHtml("<font color='#000000'>"+userName+"</font>"));
+                }
+            });
         }else{
             startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
             finish();
