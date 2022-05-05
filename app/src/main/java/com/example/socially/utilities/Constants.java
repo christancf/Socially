777d1009c0bbbcda37b1e54e0720b962f2bdec52
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-05-05 17:24:30 +0530

    Modified chat design

---
+++ app/src/main/java/com/example/socially/utilities/Constants.java
@@ -0,0 +1,12 @@
+package com.example.socially.utilities;
+
+public class Constants {
+
+    public static final String KEY_USER_ID = "userId";
+    public static final String KEY_USER = "user";
+    public static final String KEY_COLLECTION_CHAT = "chat";
+    public static final String KEY_SENDER_ID = "senderId";
+    public static final String KEY_RECEIVER_ID = "receiverId";
+    public static final String KEY_MESSAGE = "message";
+    public static final String KEY_TIMESTAMP = "timestamp";
+}
