8e84b48963b226e92aa386dfb8f584db6f041d18
Sayumi Munasinghe <it20610920@my.sliit.lk>
2022-05-06 12:56:19 +0530

    Send message function done

--- app/src/main/java/com/example/socially/activities/ChatActivity.java
+++ app/src/main/java/com/example/socially/activities/ChatActivity.java
@@ -1,17 +1,39 @@
 package com.example.socially.activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.socially.LoginActivity;
+import com.example.socially.ModelUser;
 import com.example.socially.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -23,11 +45,21 @@ public class ChatActivity extends AppCompatActivity {
     EditText messageEt;
     ImageButton sendBtn;
 
+    private String firebaseURL = "https://socially-14fd2-default-rtdb.asia-southeast1.firebasedatabase.app";
+    private Context context;
+    //firebase auth
+    FirebaseAuth firebaseAuth;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference usersDbRef;
+
+    String hisUID;
+    String myUID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-//        Intent intent = getIntent();
 
         //init views
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -39,5 +71,97 @@ public class ChatActivity extends AppCompatActivity {
         userStatusTv = findViewById(R.id.userStatusTv);
         messageEt = findViewById(R.id.messageEt);
         sendBtn = findViewById(R.id.sendBtn);
+
+        Intent intent = getIntent();
+        hisUID = intent.getStringExtra("hisUid");
+
+        //firebase auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseDatabase = FirebaseDatabase.getInstance(firebaseURL);
+        usersDbRef = firebaseDatabase.getReference("Users");
+
+        //search user to get that user's info
+        Query userQuery = usersDbRef.orderByChild("uid").equalTo(hisUID);
+        //get user picture and name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //check until required info is received
+                for (DataSnapshot ds: dataSnapshot.getChildren() ) {
+                    ModelUser modelUser = ds.getValue(ModelUser.class);
+                    //get data
+                    String name = modelUser.getFirstName() + " " + modelUser.getLastName();
+                    String image = modelUser.getProfileImage();
+                    //String name = "" + ds.child("name").getValue();
+                    //String image = "" + ds.child("image").getValue();
+
+                    //set data
+                    nameTv.setText(name);
+                    try {
+                        //image received, set it to imageview in toolbar
+                        Picasso.get().load(image).placeholder(R.drawable.ic_default_image).into(profileIv);
+                    }
+                    catch (Exception e) {
+                        //there is exception getting picture, set default picture
+                        Picasso.get().load(R.drawable.ic_default_image).into(profileIv);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //click button to send message
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //get text from edit text
+                String message = messageEt.getText().toString().trim();
+                //check if text is empty or not
+                if (TextUtils.isEmpty(message)) {
+                    Toast.makeText(ChatActivity.this, "Cannot send empty message...", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //text not empty
+                    sendMessage(message);
+                }
+            }
+        });
+    }
+
+    private void sendMessage(String message) {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance(firebaseURL).getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", myUID);
+        hashMap.put("receiver", hisUID);
+        hashMap.put("message", message);
+        databaseReference.child("Chats").push().setValue(hashMap);
+
+        //reset edittext after sending message
+        messageEt.setText("");
+    }
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null) {
+            //user signed in
+            myUID = user.getUid();
+        } else {
+            //if user not signed in, go to login page
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
     }
 }
