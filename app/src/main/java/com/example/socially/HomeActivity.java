1626c42438a53e08b61c8f615ce60f24b3df31e3
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-05-08 00:41:55 +0530

    "Added home activity functionalities"

--- app/src/main/java/com/example/socially/HomeActivity.java
+++ app/src/main/java/com/example/socially/HomeActivity.java
@@ -1,29 +1,43 @@
 package com.example.socially;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Html;
-import android.util.Log;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.socially.activities.ChatActivity;
+import com.example.socially.adapters.AdapterPost;
+import com.example.socially.models.ModelPost;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private static final String TAG = "HomeActivity";
+
     ImageView HomeIV, UsersIV, AddPostIV, ChatIV, ProfileIV;
     FirebaseAuth mAuth;
     ActionBar actionBar;
+
+    RecyclerView recyclerView;
+    List<ModelPost> postList;
+    AdapterPost adapterPost;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +55,21 @@ public class HomeActivity extends AppCompatActivity {
         AddPostIV = findViewById(R.id.addPost);
         ChatIV = findViewById(R.id.chat);
         ProfileIV = findViewById(R.id.profile);
+
+        //recycler view
+        recyclerView = findViewById(R.id.post_recycler_view);
+        LinearLayoutManager layoutManager= new LinearLayoutManager(this);
+
+        //show latest posts first
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+
+        //set layout to recycler view
+        recyclerView.setLayoutManager(layoutManager);
+
+        //init post list
+        postList = new ArrayList<>();
+        loadPosts();
 
 //        Drawable res = getResources().getDrawable(R.drawable.self_cover);
 //        ProfileIV.setImageDrawable(res);
@@ -63,5 +92,37 @@ public class HomeActivity extends AppCompatActivity {
         ProfileIV.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(), ProfileActivity.class));
         });
+    }
+
+    private void loadPosts() {
+        //path of all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        //get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for(DataSnapshot ds : snapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    postList.add(modelPost);
+
+                    //adapter
+                    adapterPost = new AdapterPost(getApplicationContext(), postList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterPost);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //in case of error
+                Toast.makeText(HomeActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void searchPosts(String searchQuery) {
+
     }
 }
