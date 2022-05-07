c7386051be6ac908af40808eca76770f49b42b21
Nimtara Munasinghe <73902228+nimtaraMunasinghe@users.noreply.github.com>
2022-05-08 03:10:05 +0530

    Implemented search post function in HomeActivity

--- app/src/main/java/com/example/socially/HomeActivity.java
+++ app/src/main/java/com/example/socially/HomeActivity.java
@@ -1,24 +1,24 @@
 package com.example.socially;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.Html;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.socially.activities.ChatlistActivity;
 import com.example.socially.adapters.AdapterPost;
 import com.example.socially.models.ModelPost;
-
-import com.example.socially.activities.ChatlistActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -51,7 +51,7 @@ public class HomeActivity extends AppCompatActivity {
         //Setting up a transparent actionbar
         actionBar = getSupportActionBar();
         actionBar.setTitle(Html.fromHtml("<font color='#000000'>Posts</font>"));
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        //actionBar.setBackgroundDrawable(new ColorDrawable(Color.WHITE));
 
         HomeIV = findViewById(R.id.home);
         UsersIV = findViewById(R.id.users);
@@ -127,5 +127,69 @@ public class HomeActivity extends AppCompatActivity {
 
     private void searchPosts(String searchQuery) {
 
+        //path of all posts
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Posts");
+        //get all data from this ref
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postList.clear();
+                for(DataSnapshot ds : snapshot.getChildren()) {
+                    ModelPost modelPost = ds.getValue(ModelPost.class);
+
+                    if(modelPost.getPostContent().toLowerCase().contains(searchQuery.toLowerCase())
+                        || modelPost.getFirstName().toLowerCase().contains(searchQuery.toLowerCase())
+                        || modelPost.getLastName().toLowerCase().contains(searchQuery.toLowerCase())) {
+                        postList.add(modelPost);
+                    }
+
+                    //adapter
+                    adapterPost = new AdapterPost(getApplicationContext(), postList);
+                    //set adapter to recycler view
+                    recyclerView.setAdapter(adapterPost);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                //in case of error
+                Toast.makeText(HomeActivity.this, ""+error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.post_menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search post...");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(!TextUtils.isEmpty(query)) {
+                    searchPosts(query);
+                } else {
+                    loadPosts();
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(!TextUtils.isEmpty(newText)) {
+                    searchPosts(newText);
+                } else {
+                    loadPosts();
+                }
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
