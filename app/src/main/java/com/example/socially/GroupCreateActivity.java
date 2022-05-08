4de354c0863dafc7ff55aace476fd3e9d4e5bf9d
SanukaJay <sanukaakmaljay@gmail.com>
2022-05-08 12:23:44 +0530

    Create group function finished

--- app/src/main/java/com/example/socially/GroupCreateActivity.java
+++ app/src/main/java/com/example/socially/GroupCreateActivity.java
@@ -1,33 +1,52 @@
 package com.example.socially;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Html;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
 
 public class GroupCreateActivity extends AppCompatActivity {
 
     //permission constants
     private static final int CAMERA_REQUEST_CODE=100;
-    private static final int STORAGE_REQUEST_CODE=100;
+    private static final int STORAGE_REQUEST_CODE=200;
 
     //image pick constants
-    private static final int IMAGE_PICK_CAMERA_CODE=200;
-    private static final int IMAGE_PICK_GALLERY_CODE=300;
+    private static final int IMAGE_PICK_CAMERA_CODE=300;
+    private static final int IMAGE_PICK_GALLERY_CODE=400;
 
     //permission arryas
     private String[] cameraPermissions;
@@ -48,6 +67,9 @@ public class GroupCreateActivity extends AppCompatActivity {
     private EditText groupDescriptionEt,groupTitleEt;
     private FloatingActionButton createGroupBtn;
 
+    private ProgressDialog progressDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,13 +79,15 @@ public class GroupCreateActivity extends AppCompatActivity {
         actionBar= getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
-        actionBar.setTitle("Create Group");
+        actionBar.setTitle(Html.fromHtml("<font color='#000000'>Create group</font>"));
 
         //init UI views
         groupIconIv=findViewById(R.id.groupIconIv);
         groupDescriptionEt=findViewById(R.id.groupDescriptionEt);
         groupTitleEt=findViewById(R.id.groupTitleEt);
         createGroupBtn=findViewById(R.id.createGroupBtn);
+
+
 
         //init permission array
         cameraPermissions = new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
@@ -84,12 +108,125 @@ public class GroupCreateActivity extends AppCompatActivity {
         createGroupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                startCreatingGroup();
 
             }
         });
 
         firebaseAuth = FirebaseAuth.getInstance();
         checkUser();
+
+
+
+
+    }
+
+    private void startCreatingGroup() {
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Creating Group");
+
+        //input  title, description
+        String groupTitle = groupTitleEt.getText().toString().trim();
+        String groupDescription = groupDescriptionEt.getText().toString().trim();
+
+        //validation
+        if(TextUtils.isEmpty(groupTitle)){
+            Toast.makeText(this, "Please enter group title...", Toast.LENGTH_SHORT).show();
+            return;
+            //dont procede further
+        }
+        progressDialog.show();
+
+        //timestamp: groupicon image,groupID,timecreated etc
+        String g_timestamp =""+System.currentTimeMillis();
+        if(image_uri==null){
+            //creating group without icon image
+            createGroup(""+g_timestamp,""+groupTitle,""+groupDescription,"");
+
+        }
+        else{
+            //creating group with icon image
+            //upload image
+            //image and name path
+            String fileNameAndPath = "Group_Imgs/" + "image"+g_timestamp;
+
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference(fileNameAndPath);
+            storageReference.putFile(image_uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    //image uploaded,get url
+                    Task<Uri> p_uriTask=taskSnapshot.getStorage().getDownloadUrl();
+                    while(!p_uriTask.isSuccessful());
+                    Uri p_downloadUri = p_uriTask.getResult();
+                    if(p_uriTask.isSuccessful()){
+                        createGroup(""+g_timestamp,""+groupTitle,""+groupDescription,""+p_downloadUri);
+                    }
+
+
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            //failed uploading image
+                            progressDialog.dismiss();
+                            Toast.makeText(GroupCreateActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void createGroup(String g_timestamp,String groupTitle,String groupDescription,String groupIcon) {
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("groupId",""+g_timestamp);
+        hashMap.put("groupTitle",""+groupTitle);
+        hashMap.put("groupDescription",""+groupDescription);
+        hashMap.put("groupIcon",""+groupIcon);
+        hashMap.put("timestamp",""+g_timestamp);
+        hashMap.put("createBy",""+firebaseAuth.getUid());
+
+        //create group
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Groups");
+        ref.child(g_timestamp).setValue(hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                //created successfully
+
+                //setup member inf(add current user in group's participants list)
+                HashMap<String,String> hashMap1 = new HashMap<>();
+                hashMap1.put("uid",firebaseAuth.getUid());
+                hashMap1.put("role","creator");
+                hashMap1.put("timestamp",g_timestamp);
+
+                DatabaseReference ref1 = FirebaseDatabase.getInstance().getReference("Groups");
+                ref1.child(g_timestamp).child("participants").child(firebaseAuth.getUid()).setValue(hashMap1).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        //participants added
+                        progressDialog.dismiss();
+                        Toast.makeText(GroupCreateActivity.this, "Group Created...", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //failed adding participants
+                        
+                        progressDialog.dismiss();
+                        Toast.makeText(GroupCreateActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //falied
+                        progressDialog.dismiss();
+                        Toast.makeText(GroupCreateActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
 
 
 
@@ -108,10 +245,21 @@ public class GroupCreateActivity extends AppCompatActivity {
                 //handle clicks
                     if(which==0){
                         //camera clicked
+                        if(!checkCameraPermissions()){
+                            requestCameraPermissions();
+                        }
+                        else{
+                            pickFromCamera();
+                        }
                     }
                     else{
                         //gallery clicked
-
+                        if(!checkStoragePermissions()){
+                            requestStoragePermissions();
+                        }
+                        else{
+                            pickFromGallery();
+                        }
                     }
                 }
             }).show();
@@ -140,8 +288,25 @@ public class GroupCreateActivity extends AppCompatActivity {
     }
 
     private boolean checkStoragePermissions(){
-        //change this
-        return true;
+        boolean result = ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result;
+    }
+
+
+
+    private void requestStoragePermissions(){
+        ActivityCompat.requestPermissions(this,storagePermissions,STORAGE_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermissions(){
+        boolean result = ContextCompat.checkSelfPermission(this,Manifest.permission.CAMERA)== (PackageManager.PERMISSION_GRANTED);
+        boolean result1= ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)==(PackageManager.PERMISSION_GRANTED);
+
+        return result && result1;
+    }
+
+    private void requestCameraPermissions(){
+        ActivityCompat.requestPermissions(this,cameraPermissions,CAMERA_REQUEST_CODE);
     }
 
     private void checkUser(){
@@ -155,5 +320,66 @@ public class GroupCreateActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //handle permission result
+        switch (requestCode){
+            case CAMERA_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(cameraAccepted && storageAccepted){
+                        //permission allowed
+                        pickFromCamera();
+                    }else{
+                        //both or one is denied
+                        Toast.makeText(this, "Camera & Storage permissions are required", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+            }
+
+            break;
+            case STORAGE_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    boolean storageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    if(storageAccepted){
+                        //permission allowed
+                        pickFromGallery();
+                    }
+                    else{
+                        //permission denied
+                        Toast.makeText(this, "Storage permissions required", Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                }
+            }
+
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        //hanlde image pick result
+        if(resultCode==RESULT_OK){
+            if(requestCode== IMAGE_PICK_GALLERY_CODE){
+                //was picked from gallery
+                image_uri = data.getData();
+                //set to image view
+                groupIconIv.setImageURI(image_uri);
+            }
+            else if(requestCode== IMAGE_PICK_CAMERA_CODE){
+                //was picked from camera
+
+                //set to image view
+                groupIconIv.setImageURI(image_uri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
