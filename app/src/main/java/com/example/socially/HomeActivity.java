b3c89c511a39619ce4095433cdd624ded357fbed
christancf <christanf6@gmail.com>
2022-05-08 21:42:06 +0530

    Bottom navbar modified

--- app/src/main/java/com/example/socially/HomeActivity.java
+++ app/src/main/java/com/example/socially/HomeActivity.java
@@ -24,7 +24,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,6 +44,7 @@ public class HomeActivity extends AppCompatActivity {
     AdapterPost adapterPost;
 
     private String firebaseURL = "https://socially-14fd2-default-rtdb.asia-southeast1.firebasedatabase.app";
+    String userProfilePicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +64,7 @@ public class HomeActivity extends AppCompatActivity {
         ChatIV = findViewById(R.id.chat);
         ProfileIV = findViewById(R.id.profile);
 
+        setupUserProfileImage();
         //recycler view
         recyclerView = findViewById(R.id.post_recycler_view);
         LinearLayoutManager layoutManager= new LinearLayoutManager(this);
@@ -193,5 +197,29 @@ public class HomeActivity extends AppCompatActivity {
         });
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setupUserProfileImage() {
+        DatabaseReference db = FirebaseDatabase.getInstance(firebaseURL).getReference("Users");
+        Query query = db.orderByChild("uid").equalTo(mAuth.getCurrentUser().getUid());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot ds: snapshot.getChildren()) {
+                    userProfilePicture = "" + ds.child("profileImage").getValue();
+                }
+                try {
+                    Picasso.get().load(userProfilePicture).into(ProfileIV);
+                }catch (Exception e){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
